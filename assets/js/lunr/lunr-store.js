1b111edbf72447ca2c348b916d5b2ddf8d425384
minjae.kim96@gmail.com <minjae.kim96@gmail.com>
2019-05-04 03:54:23 +0000

    Deploy from Forestry.io

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,41 +1,16 @@
 var store = [{
-        "title": "Content",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "www.kimmj.com/content/",
+        "title": "Android Camera2 API Pipeline Manipulation",
+        "excerpt":"Intro Ever since the birth of digital cameras, the ability to edit its image processing pipeline has been held exclusively by the camera manufacturers. Google is trying to change the game with the release of the Camera 2 API for Android. This new program interface will enable developers to input...","categories": ["code"],
+        "tags": ["Android","Camera","OpenCV","Computer Vision","White Balance"],
+        "url": "www.kimmj.com/code/android-camera2-api-pipeline-app/",
         "teaser":null},{
-        "title": "Content",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "www.kimmj.com/content/",
+        "title": "Sitecore Hackathon 2019 with Apiqu!",
+        "excerpt":"   Sitecore Hackathon Website   Our Hackathon Submission  ","categories": ["code"],
+        "tags": ["Sitecore","Hackathon","SEO","Sitecore Addon","Marketing","Content Editing","CPC"],
+        "url": "www.kimmj.com/code/sitecore-hackathon-2019-with-apiqu/",
         "teaser":null},{
-        "title": "Content",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "www.kimmj.com/content/",
-        "teaser":null},{
-        "title": "Content",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "www.kimmj.com/content/",
-        "teaser":null},{
-        "title": "Content",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "www.kimmj.com/content/",
-        "teaser":null},{
-        "title": "Content",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "www.kimmj.com/content/",
-        "teaser":null},{
-        "title": "Content",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "www.kimmj.com/content/",
-        "teaser":null},{
-        "title": "Sitecore Hackathon 2019 With Apiqu",
-        "excerpt":"http://www.sitecorehackathon.org/sitecore-hackathon-2019/ https://github.com/Sitecore-Hackathon/2019-The-Knights-Who-Say-Ni  ","categories": [],
-        "tags": [],
-        "url": "www.kimmj.com/sitecore-hackathon-2019-with-apiqu/",
+        "title": "Sitecore 9.0 Certification Exam and Preparation",
+        "excerpt":"Sitecore is a web content management system (CMS) with extensive marketing and personalization features. With enterprise-grade websites like Procter &amp; Gamble, Huawei, LastPass, and many more, Sitecore is a valuable technology to learn as a web or full-stack developer. I received a great opportunity to study and write the Sitecore®...","categories": ["code"],
+        "tags": ["sitecore","certification","exam","preparation","developer"],
+        "url": "www.kimmj.com/code/sitecore-9-0-certification-exam-and-preparation/",
         "teaser":null}]
