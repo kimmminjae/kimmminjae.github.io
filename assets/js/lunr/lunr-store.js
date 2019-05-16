6f4bbdc4a3cb6147dbe151dc61133e532a43a4e3
minjae.kim96@gmail.com <minjae.kim96@gmail.com>
2019-05-16 17:02:12 +0000

    Deploy from Forestry.io

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -24,7 +24,7 @@ var store = [{
         "tags": ["coveo","certification","sitecore","coveo hive","exam","training"],
         "url": "www.kimmj.com/blog/coveo-for-sitecore-4-1-hive-certification-process/",
         "teaser":null},{
-        "title": "Easy Sitecore 9.1 Installation Guide - Without Command Line, from Fresh Windows Install",
+        "title": "Easy Sitecore 9.1 Installation Guide",
         "excerpt":"Relevant Links Sitecore 9.1.0 XP0 Single On Premises Sitecore 9.1.0 XP0 installation media https://dev.sitecore.net/Downloads/Sitecore_Experience_Platform/91/Sitecore_Experience_Platform_91_Initial_Release.aspx Sitecore License If you or your employer doesn’t have a Sitecore License, you can get a 60 day trial license here: https://www.sitecore.com/getting-started/developer-trial SQL Server 2017 (or 2016 SP2 or Greater) https://www.microsoft.com/en-us/sql-server/sql-server-downloads Java Runtime Environment (JRE) https://www.java.com/en/download/...","categories": ["blog"],
         "tags": ["Sitecore 9.1","Sitecore Installation","Installation Guide","Sitecore","Sitecore Experience Platform 9.1","SIF-Less"],
         "url": "www.kimmj.com/blog/easy-sitecore-9-1-installation-guide-fresh-windows-install-without-command-lines/",
